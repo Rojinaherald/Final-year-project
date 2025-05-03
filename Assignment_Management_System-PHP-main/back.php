57b56c859f69025d7e274c58b2f59a20ded8f087
Rojinaherald <119416202+Rojinaherald@users.noreply.github.com>
2025-05-03 20:44:59 +0545

    Removed old_file.php from project

--- Assignment_Management_System-PHP-main/back.php
+++
@@ -1,3 +0,0 @@
-<script type="text/javascript">
-	window.location = 'view-users.php';	
-</script>
--- Assignment_Management_System-PHP-main/db_handler.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('includes/config.php');
-
-$conn = mysqli_connect(db_host, db_username, db_password, db_name);
-
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
